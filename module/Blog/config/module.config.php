e1fdd231189f8f3f7050aad826cf6938e35cca52
Kevin Bulloch <kevinb93@hotmail.co.uk>
2021-01-31 20:14:03 +0000

    configure blog router

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -4,6 +4,7 @@ namespace Blog;
 
 use Laminas\ServiceManager\Factory\InvokableFactory;
 use Laminas\Router\Http\Literal;
+use Laminas\Router\Http\Segment;
 
 return [
     'service_manager' => [
@@ -20,23 +21,30 @@ return [
             Controller\ListController::class => Factory\ListControllerFactory::class,
         ],
     ],
-    // This lines opens the configuration for the RouteManager
-    'router' => [
-        // Open configuration for all possible routes
+    'router'          => [
         'routes' => [
-            // Define a new route called "blog"
             'blog' => [
-                // Define a "literal" route type:
                 'type' => Literal::class,
-                // Configure the route itself
                 'options' => [
-                    // Listen to "/blog" as uri:
-                    'route' => '/blog',
-                    // Define default controller and action to be called when
-                    // this route is matched
+                    'route'    => '/blog',
                     'defaults' => [
                         'controller' => Controller\ListController::class,
                         'action'     => 'index',
+                    ],
+                ],
+                'may_terminate' => true,
+                'child_routes'  => [
+                    'detail' => [
+                        'type' => Segment::class,
+                        'options' => [
+                            'route'    => '/:id',
+                            'defaults' => [
+                                'action' => 'detail',
+                            ],
+                            'constraints' => [
+                                'id' => '[1-9]\d*',
+                            ],
+                        ],
                     ],
                 ],
             ],
