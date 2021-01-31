185ff1afbe77df41508141722dc6aac28dde4a29
Kevin Bulloch <kevinb93@hotmail.co.uk>
2021-01-31 13:28:04 +0000

    add forms

--- config/modules.config.php
+++ config/modules.config.php
@@ -12,6 +12,12 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Mvc\I18n',
+    'Laminas\I18n',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Form',
     'Laminas\Db',
     'Laminas\Router',
     'Laminas\Validator',
