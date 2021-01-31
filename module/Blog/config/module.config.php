547bb8db3bd1181808b9f0425171f10356c10ea8
Kevin Bulloch <kevinb93@hotmail.co.uk>
2021-01-31 20:48:22 +0000

    add blog post CRUD actions

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -10,15 +10,20 @@ return [
     'service_manager' => [
         'aliases' => [
             Model\PostRepositoryInterface::class => Model\LaminasDbSqlRepository::class,
+            Model\PostCommandInterface::class => Model\LaminasDbSqlCommand::class,
         ],
         'factories' => [
             Model\PostRepository::class => InvokableFactory::class,
             Model\LaminasDbSqlRepository::class => Factory\LaminasDbSqlRepositoryFactory::class,
+            Model\PostCommand::class => InvokableFactory::class,
+            Model\LaminasDbSqlCommand::class => Factory\LaminasDbSqlCommandFactory::class,
         ],
     ],
     'controllers' => [
         'factories' => [
             Controller\ListController::class => Factory\ListControllerFactory::class,
+            Controller\WriteController::class => Factory\WriteControllerFactory::class,
+            Controller\DeleteController::class => Factory\DeleteControllerFactory::class,
         ],
     ],
     'router'          => [
@@ -40,6 +45,42 @@ return [
                             'route'    => '/:id',
                             'defaults' => [
                                 'action' => 'detail',
+                            ],
+                            'constraints' => [
+                                'id' => '\d+',
+                            ],
+                        ],
+                    ],
+                    'add' => [
+                        'type' => Literal::class,
+                        'options' => [
+                            'route'    => '/add',
+                            'defaults' => [
+                                'controller' => Controller\WriteController::class,
+                                'action'     => 'add',
+                            ],
+                        ],
+                    ],
+                    'edit' => [
+                        'type' => Segment::class,
+                        'options' => [
+                            'route'    => '/edit/:id',
+                            'defaults' => [
+                                'controller' => Controller\WriteController::class,
+                                'action'     => 'edit',
+                            ],
+                            'constraints' => [
+                                'id' => '[1-9]\d*',
+                            ],
+                        ],
+                    ],
+                    'delete' => [
+                        'type' => Segment::class,
+                        'options' => [
+                            'route' => '/delete/:id',
+                            'defaults' => [
+                                'controller' => Controller\DeleteController::class,
+                                'action'     => 'delete',
                             ],
                             'constraints' => [
                                 'id' => '[1-9]\d*',
---
+++ module/Blog/src/Form/PostFieldSet.php
@@ -0,0 +1,36 @@
+<?php
+namespace Blog\Form;
+
+use Blog\Model\Post;
+use Laminas\Hydrator\ReflectionHydrator;
+use Laminas\Form\Fieldset;
+
+class PostFieldset extends Fieldset
+{
+    public function init()
+    {
+        $this->setHydrator(new ReflectionHydrator());
+        $this->setObject(new Post('', ''));
+
+        $this->add([
+            'type' => 'hidden',
+            'name' => 'id',
+        ]);
+
+        $this->add([
+            'type' => 'text',
+            'name' => 'title',
+            'options' => [
+                'label' => 'Post Title',
+            ],
+        ]);
+
+        $this->add([
+            'type' => 'textarea',
+            'name' => 'text',
+            'options' => [
+                'label' => 'Post Text',
+            ],
+        ]);
+    }
+}
